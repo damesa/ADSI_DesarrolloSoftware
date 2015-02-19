435ffd383cce9189ba245bceaadbc80820551697
Diego Mesa <diegomesa.1414@gmail.com>
2015-02-19 16:01:04 -0500

    Tabajo_2, Creado clase  Tbla

--- Trabajo_2/index.php
+++ Trabajo_2/index.php
@@ -4,9 +4,33 @@
 	<title>Graficas</title>
 </head>
 <body>
-<h1>Grafic1</h1>
-<img src="pie.php">
+<h1>Grafica1</h1>
+<?php 
+	//mostrar errores true
+ ini_set('display_errors', true);
+ //mostrar todos los errores
+ error_reporting(E_ALL);
+	require 'Tabla.php';
+	$tabla1 = new Tabla(
+			"Resultado del uso de instrumentos musicales",
+			"cual es el mas usado",
+			"450",
+			"Esta vaina es de mentiras",
+			"pie"
+		);
+	echo $tabla1->dibujar();
+ ?>
 <h1>Grafica2</h1>
 <img src="bar.php">
 </body>
 </html>
+
+<div class="grafica">
+	<h1 class="titulo"></h1>
+	<h2 class="subtitulo"></h2>	
+	<div class="descripcon"></div>
+	<figure>
+	<img src="">
+	</figure>
+	<p class="nota"></p>
+</div>
