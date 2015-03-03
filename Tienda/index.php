add1d87664195852b3db6117ace3f67768cd4199
Diego Mesa <diegomesa.1414@gmail.com>
2015-03-03 01:39:04 -0500

    Tienda , Generar lista de Productos
    Los datos del formulario se guardan en un array , el cual es convertido
    a formato JSON  y este es puesto en el formulario, para asi cuando se
    haga el envio del formulario este lleve con sigo los articulos antes
    agregados

--- Tienda/index.php
+++ Tienda/index.php
@@ -4,18 +4,20 @@ date_default_timezone_set("America/Bogota") ;
 //si no se a enviado
 if (!isset($_POST['agregar'])):
 	$_POST['total'] = 0;
-	$_POST['articulos'] = array();
+	$_POST['lista'] = array();
 else:
 	//datos enviados
-	$nombre   = $_POST['nombre'];
-	$cantidad = $_POST['cantidad'];
-	$precio   = $_POST['precio'];
-
-	$rubro = $cantidad * $precio;
-	$_POST['total'] += $rubro;
+	 $nombre   = $_POST['nombre'];
+	 $cantidad = $_POST['cantidad'];
+	 $precio   = $_POST['precio'];
+ 
+	 $rubro = $cantidad * $precio;
+	 $_POST['total'] += $rubro;
 
 	//presintencia de datos
-	$art =  array('n' =>$nombre,'c'=>$cantidad,'p'=>$precio);
+	 $arr = array();
+	 $arr = json_decode($_POST['lista']);
+	 $arr[] = compact("nombre","cantidad","precio");
 
 endif;
 
@@ -38,12 +40,22 @@ endif;
 		<input type="number" name="precio" placeholder="Precio" min="50" required>
 		<input type="number" name="cantidad" placeholder="Cantidad" min="1" required>
 		<span><p>Total: </p></span>
-		<input type="number" name="total" value="<?= $_POST['total']  ?>">
+		<input type="number" name="total" value="<?= $_POST['total']?>">
+		<?php $json = json_encode($arr) ?>
+ 		<input name="lista" value='<?=$json?>' style="display:none">
 		<input type="submit" name="agregar" value="Agregar">
 	</form>
 </section>
 <section class="lista">
-	
+	<ul>
+		<?php foreach ($arr as $key => $item): ?>
+			<li id="item_<?=$key?>">
+				<?php foreach ($item as $campo => $valor):?>
+					<input type="text" name="<?=$campo?>" value="<?=$valor?>">
+				<?php endforeach; ?>
+			</li>
+		<?php endforeach;?>
+	</ul>
 </section>
 </body>
 </html>
