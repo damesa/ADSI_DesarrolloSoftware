bdf64b3c6bc086b524c79861217f9b5becb6cd3c
damesa <diegomesa.1414@gmail.com>
2015-03-05 11:53:59 -0500

    Tienda _ removidos Archivos , solo dejo Tienda/

--- Trabajo_2/controller/generar.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-	$_POST['imagen'] = "../../Trabajo_2_Demo/img/yo.php";
-
-	CargarView('lienzo');
- ?>
--- Trabajo_2/view/404.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include 'header.tpl.php';
-
-
-echo "pagina no encontrada";
-
-
-include 'footer.tpl.php';
-?>
