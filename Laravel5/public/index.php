dbb8ea340fc81eb78b05f215380e69a2ca90b3d0
damesa <diegomesa.1414@gmail.com>
2015-03-08 19:48:14 +0000

    Laravel5, Gererar pdf con dompdf . edit document root

---
+++ Laravel5/public/index.php
@@ -0,0 +1,36 @@
+<?php
+
+// somewhere early in your project's loading, require the Composer autoloader
+// see: http://getcomposer.org/doc/00-intro.md
+require '../vendor/autoload.php';
+
+// disable DOMPDF's internal autoloader if you are using Composer
+define('DOMPDF_ENABLE_AUTOLOAD', false);
+
+// include DOMPDF's default configuration
+require_once '../vendor/dompdf/dompdf/dompdf_config.inc.php';
+
+// generar php
+$html =
+  '<html><body>'.
+    '<h1>Duilio.me</h1>'.
+    '<h2>dompdf</h2>'.
+  '<p>Aprendindo composer del curso de laravel5</p>'.
+  '</body></html>';
+
+$dompdf = new DOMPDF();
+$dompdf->load_html($html);
+$dompdf->render();
+$dompdf->stream("sample.pdf");
+$html =
+  '<html><body>'.
+  '<p>Put your html here, or generate it with your favourite '.
+  'templating system.</p>'.
+  '</body></html>';
+
+$dompdf = new DOMPDF();
+$dompdf->load_html($html);
+$dompdf->render();
+$dompdf->stream("sample.pdf");
+
+?>
