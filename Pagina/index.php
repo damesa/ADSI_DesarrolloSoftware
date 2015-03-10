b97e0d32c0cbac1bd5357f3cbce8650f12e70bd0
damesa <diegomesa.1414@gmail.com>
2015-03-10 04:11:25 +0000

    Estableciend pagina para Portafolio Online
    Pagina principal enlaza al los proyectos , boton de regresar al home en todas los proyectos

---
+++ Pagina/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this template use Tools | Templates.
+ */
+?>
+<html>
+<head>
+    <title>Damesa</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.2/normalize.min.css">
+</head>
+<body>
+ <header>
+ </header>
+ <section>
+    <div class="card">
+        <figure>
+            <img src="/" alt="">
+        </figure>
+        <h1>Pymestnio</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. autem dicta ea iste.</p>
+        <p><a href="#">link</a></p>
+        <a href="index.php?url=pymestion" class="more">Read More</a>
+    </div> 
+ </section>
+    <link rel="stylesheet" href="Pagina/css/stylus.css">
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
 
-require "Tienda/index.php"
+//require "Tienda/index.php"
+
+if(isset($_GET['url'])){
+    if($_GET['url']=='pymestion'){
+        require "Tienda/index.php";
+    }
+    else if($_GET['url']=='cursogit'){
+        require "Cursos/index.php?url=cursogit";
+    }
+}
+else{
+    require "Pagina/index.php";
+}
 ?>
+<style>
+    #damesa{
+        position: absolute;
+        right: 2rem;
+        top: 2rem;
+        background: #5AFD7B;
+        padding: 1rem;
+        border-radius: 10px;
+        color: #fff;
+    }
+</style>
+<div id="damesa"><a href="index.php">Damesa.com</a></div>
+
+
